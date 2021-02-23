6537b7db675a78c974035947010a8ec5cbaecc71
Chaashya <fernandochaashya@gmail.com>
2021-02-23 21:40:23 +1000

    started view side

---
+++ sms/resources/views/studentList.blade.php
@@ -0,0 +1,22 @@
+<div class="col">
+<table class="table">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">CNE</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Surname</th>
+      <th scope="col">Age</th>
+      <th scope="col">Specialty</th>
+      <th scope="col">Operations</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>
